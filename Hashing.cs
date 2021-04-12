73631c6cc62170f6dbd144ccb812698919e53620
AustinOnshore <81576463+AustinOnshore@users.noreply.github.com>
2021-04-11 22:15:39 -0400

    Add files via upload

---
+++ Hashing.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+
+namespace Afternoon_4_5_21
+{
+    class Hashing
+    {
+
+
+        public string ComputeSHA256Hash(string rawData)
+        {
+            string value;
+
+            using (SHA256 sha = SHA256.Create())
+            {
+
+                byte[] bytes = sha.ComputeHash(Encoding.UTF8.GetBytes(rawData));
+
+                StringBuilder builder = new StringBuilder();
+
+                for (int i = 0; i < bytes.Length; i++)
+                {
+
+                    builder.Append(bytes[i].ToString("x2"));
+
+                }
+                value = builder.ToString();
+            }
+
+                return value;
+
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Afternoon_4_5_21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Hashing h = new Hashing();
+
+            Console.WriteLine("Enter your string to hash.");
+
+            string _input = Console.ReadLine();
+
+            string s = h.ComputeSHA256Hash(_input);
+
+            Console.WriteLine("The hash is: " + s);
+
+            Console.ReadKey();
+
+
+
+
+
+
+
+        }
+    }
+}
